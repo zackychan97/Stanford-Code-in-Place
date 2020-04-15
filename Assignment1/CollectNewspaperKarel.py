21c468a19a1a45272084f2201a266421ce91e8c1
Zack Bumpous <upwork.ztb@gmail.com>
2020-04-15 14:44:07 -0500

    Got TripleKarel.py to get the beeper and bring it back to original spot. Still will need to eventually get it to work and not crash in other worlds. But, for now, I am moving on to the first Problem of our assignment, which will be inside TripleKarel.py

--- Assignment1/CollectNewspaperKarel.py
+++ Assignment1/CollectNewspaperKarel.py
@@ -18,9 +18,54 @@ def main():
     starting to write your own code. You should also delete this
     comment and replace it with a better, more descriptive one.
     """
-    pass
+    move()
+    triple_turn_left()
+    move()
+    turn_left()
+    double_jump()
+    pick_beeper()
+    double_left_turn()
+    triple_jump()
+    triple_turn_left()
+    move()
+    triple_turn_left()
 
 
+
+# Original World get Newspaper Function
+# def get_the_news():
+#     while front_is_clear():
+#         move()
+#         triple_turn_left()
+#         move()
+#         turn_left()
+#         double_jump()
+#         pick_beeper()
+#         double_left_turn()
+#         triple_jump()
+#         triple_turn_left()
+#         move()
+#         triple_turn_left()
+
+# Turns
+def triple_turn_left():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def double_left_turn():
+    turn_left()
+    turn_left()
+
+# Jumps
+def double_jump():
+    move()
+    move()
+
+def triple_jump():
+    move()
+    move()
+    move()
 # There is no need to edit code beyond this point
 
 if __name__ == "__main__":
