05bfcb8364437e61a3d52e7b1305c42f62e92305
Zack Bumpous <upwork.ztb@gmail.com>
2020-04-27 20:04:47 -0500

    Adds code into khansole_academy but it is not complete. Just want my code on github incase I lost it while exiting PyCharm.

--- Assignment2/khansole_academy.py
+++ Assignment2/khansole_academy.py
@@ -6,6 +6,12 @@ Add your comments here.
 
 import random
 
+PROBLEMS_REQUIRED = 3
+MIN_RANDOM = 1
+MAX_RANDOM = 99
+RANDOM_NUMBER1 = random.randint(MIN_RANDOM, MAX_RANDOM)
+RANDOM_NUMBER2 = random.randint(MIN_RANDOM, MAX_RANDOM)
+ENTER_ANSWER = int(input("Enter the answer. "))
 
 def main():
     """
@@ -14,7 +20,19 @@ def main():
     your own code. You should also delete this comment and replace
     it with a better, more descriptive one.
     """
-    pass
+    teach_addition()
+
+def teach_addition():
+    print("What is " + str(RANDOM_NUMBER1) + " + " + str(RANDOM_NUMBER2) + "?")
+    ENTER_ANSWER
+    if ENTER_ANSWER == RANDOM_NUMBER1 + RANDOM_NUMBER2:
+
+def ask_and_enter():
+    for i in range(2):
+        print("What is " + str(RANDOM_NUMBER1) + " + " + str(RANDOM_NUMBER2) + "?")
+        ENTER_ANSWER
+
+
 
 
 # This provided line is required at the end of a Python file
