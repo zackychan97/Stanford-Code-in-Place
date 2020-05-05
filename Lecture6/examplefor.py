847a870118f72c28b03512e6964e193467034ab2
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-05 12:11:52 -0500

    examplefor.py now prints the first 100 even numbers, beginning at 2 and ending at 198.

--- Lecture6/examplefor.py
+++ Lecture6/examplefor.py
@@ -5,8 +5,8 @@ Print the first 100 even numbers
 """
 
 def main():
-    for i in range(12):
-        print(i)
+    for i in range(100):
+        print(i * 2)
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
