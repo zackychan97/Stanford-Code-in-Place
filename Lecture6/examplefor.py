32bd6d8e678e097aaee19b775ec33c620ea93615
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-05 12:15:40 -0500

    Adds an alternative_solution function I defined that used a different way of setting the range in the for loop.

--- Lecture6/examplefor.py
+++ Lecture6/examplefor.py
@@ -8,6 +8,11 @@ def main():
     for i in range(100):
         print(i * 2)
 
+# Alternative solution that takes in a different range.
+# 0 = start here, 200 = stop before here, 2 = skip by this much each time.
+def alternative_solution():
+    for i in range(0, 200, 2):
+        print(i)
 # This provided line is required at the end of a Python file
 # to call the main() function.
 if __name__ == '__main__':
