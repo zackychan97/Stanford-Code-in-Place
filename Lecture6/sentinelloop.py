5cbc0b288f46a83293823dab35b0359777c98b83
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-05 11:40:14 -0500

    Completes sentinelloop.py inside of Lecture6. This had a fencepost issue where we had an extra piece of code, in this case setting a variable assigned to an input so a while loop will only run under that certain condition. This python file will simulate rolling two dice, and prints results of each roll as well as the total.

--- Lecture6/sentinelloop.py
+++ Lecture6/sentinelloop.py
@@ -4,10 +4,21 @@ File: sentinelloop.py
 Simulate rolling two dice, and prints results of each
 roll as well as the total.
 """
+import random
 
 def main():
     total = 0
-    print(total)
+    print("We are going to roll two dice.")
+    roll = str(input("Type: 'Roll' to roll the dice, type anything else to quit. "))
+    while roll == "Roll":
+        dice1 = random.randint(1, 6)
+        print("Dice 1: ", dice1)
+        dice2 = random.randint(1, 6)
+        print("Dice 2: ", dice2)
+        total = total + dice1 + dice2
+        print("Total: ", total)
+        roll = str(input("Type: 'Roll' to roll the dice, type anything else to quit. "))
+
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
