d9e223ea44fb630a8e6ac881d0187fc3d104d3a3
Zack Bumpous <upwork.ztb@gmail.com>
2020-05-08 20:18:41 -0500

    Adds code and some image files to play with. You will just have to change what image SimpleImage is referring to when it is assigned to the variable in the grey_scale function. The code itself will get the average RGB pixel value, and if it is greater than 153, it will set the individual pixels to the average. This is more so just to play around and get feet wet with Images in python. You may have to use this command in your terminal to get this to work, for windows: py -m pip install Pillow and then for mac/bash shell: python3 -m pip install Pillow

---
+++ Section-4-Handout/section_filter.py
@@ -0,0 +1,25 @@
+from simpleimage import SimpleImage
+
+def main():
+    """
+    This program loads an image and applies the narok filter
+    to it by setting "bright" pixels to grayscale values.
+    """
+    grey_scale()
+
+def get_pixel_average(pixel):
+    avg = (pixel.red + pixel.green + pixel.blue) // 3
+    return avg
+
+def grey_scale():
+    image = SimpleImage('images/flower.png')
+    for pixel in image:
+        average = get_pixel_average(pixel)
+        if average > 153:
+            pixel.red = average
+            pixel.blue = average
+            pixel.green = average
+    image.show()
+
+if __name__ == '__main__':
+    main()
